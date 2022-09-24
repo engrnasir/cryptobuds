89661a1b6510d9f1a2a6349acfa88e3673e52bda
Nasir Raza <iinasirraza@gmail.com>
2022-09-25 01:52:42 +0500

    mixed Sender updated

--- src/components/ProgressComp.vue
+++ src/components/ProgressComp.vue
@@ -60,9 +60,10 @@ $grey:desaturate($colour,90%);
       stroke: #5B5947;        
       outline: 12px solid #1A1919;
       box-shadow: 0px 0px 5px #F3AD26;
-      border-radius: 50%;
       -webkit-border-radius: 50%;
       -moz-border-radius: 50%;
+      border-radius: 50%;
+      -khtml-border-radius: 50%;
       width: min-content;
       box-shadow: 0 0 10px 10px #fabd44;
     }
